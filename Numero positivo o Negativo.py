36d6d6168ceebc4c1c5e0b4eee9b7a4070ecf90c
Naomi Muñoz <em987108@gmail.com>
2025-01-07 17:05:08 -0500

    Determina si es positivo, negativo o cero_Ejercicio_2

---
+++ Numero positivo o Negativo.py
@@ -0,0 +1,12 @@
+"Numero positivo o Negativo"
+# Solicitar un número al usuario
+numero = float(input("Ingresa un número: "))
+
+# Verificar si el número es positivo, negativo o cero
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
+
