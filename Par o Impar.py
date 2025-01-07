aa589b09f17f4208b66df1d60250cf848788a615
Naomi Muñoz <em987108@gmail.com>
2025-01-07 17:07:38 -0500

    Determina si es par o impar_Ejercicio_3

---
+++ Par o Impar.py
@@ -0,0 +1,9 @@
+"Par o Impar"
+# Solicitar un número al usuario
+numero = int(input("Ingresa un número: "))
+
+# Determinar si es par o impar
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
