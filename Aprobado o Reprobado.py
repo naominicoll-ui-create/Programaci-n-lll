8c00b6cfbc0cdbbc4a135b8ed1bfe2d44ca1a276
Naomi Muñoz <em987108@gmail.com>
2025-01-07 17:09:56 -0500

    Determina si el estudiante esta aprobado o reprobado_Ejercicio_4

---
+++ Aprobado o Reprobado.py
@@ -0,0 +1,9 @@
+"Aprobado o Reprobado"
+# Solicitar la calificación al usuario
+calificacion = float(input("Ingresa la calificación del estudiante: "))
+
+# Determinar si está aprobado o reprobado
+if calificacion >= 7:
+    print("El estudiante está aprobado.")
+else:
+    print("El estudiante está reprobado.")
