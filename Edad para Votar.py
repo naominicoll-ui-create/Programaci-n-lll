f9643538904fa98f9435cb307d1df4fd387caa16
Naomi Muñoz <em987108@gmail.com>
2025-01-07 17:14:42 -0500

    Determina si es legible para votar_Ejercicio_6

---
+++ Edad para Votar.py
@@ -0,0 +1,9 @@
+"Edad para Votar"
+# Solicitar la edad del usuario
+edad = int(input("Ingresa tu edad: "))
+
+# Determinar si es elegible para votar
+if edad >= 18:
+    print("Eres elegible para votar.")
+else:
+    print("No eres elegible para votar.")
